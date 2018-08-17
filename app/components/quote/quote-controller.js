7463fe15e802078974271755164a752112946bb7
Madison Abegglen <madison.abegglen@gmail.com>
2018-08-17 10:17:47 -0600

    did the quote

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -4,10 +4,15 @@ let qs = new QuoteService
 
 
 function draw(quote) {
-	console.log(quote)
-
-
+	let template = `
+	<div class="col-sm-3">
+		<h2> QOTD: </h2>
+		<h4>"${quote.quote}"</h4>
+		<h4>-${quote.author}</h4>	
+	</div>
+	`
 	//template stuff here
+	document.getElementById('quote').innerHTML = template;
 }
 
 export default class QuoteController {
